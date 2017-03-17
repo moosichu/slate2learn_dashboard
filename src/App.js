8ab2d888dfe18533e123529807e9b20a45563266
Christian Silver <christian@decode.org.nz>
2017-03-17 13:48:13 +0000

    Add rendering of experiences

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { content } from './dataset';
-
-console.log(content);
+import { experiences } from './dataset';
 
 class App extends Component {
   render() {
@@ -16,6 +14,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          {experiences.map(({ question_id, score }) => (
+            <div>{question_id}: {score}</div>
+          ))}
+        </div>
       </div>
     );
   }
