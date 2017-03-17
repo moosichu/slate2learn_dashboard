2c722a9d4d3034ef274c542d3aa130984fe1d69f
Christian Silver <christian@decode.org.nz>
2017-03-17 13:40:27 +0000

    Make dataset available in JS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { content } from './dataset';
+
+console.log(content);
 
 class App extends Component {
   render() {
---
+++ src/dataset/index.js
@@ -0,0 +1,5 @@
+export { default as content } from './content';
+export { default as experiences } from './experiences';
+export { default as learners } from './learners';
+export { default as memories } from './memories';
+export { default as transactions } from './transactions';
